e8e72204488effc77b546ad2fd9e0a77ecb50d48
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-08 14:33:30 +0300

    modify root layout

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import ConvexClerkProvider from "./providers/convexClerkProvider";
+export const metadata: Metadata = {
+  title: "Podcaster",
+  description: "Generate you podcasts using AI",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>
+        <ConvexClerkProvider>{children}</ConvexClerkProvider>
+      </body>
+    </html>
+  );
+}
