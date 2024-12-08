69b3042929e76de27f154efe7fab24be93034f55
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-08 14:19:22 +0300

    add clerk auth config

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://one-malamute-30.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+export default authConfig;
