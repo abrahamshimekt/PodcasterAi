7b2dcd5f39b6650169bec90c404632cdb1203da2
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-08 14:22:00 +0300

    add constants

---
+++ constants/index.ts
@@ -0,0 +1,98 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/icons/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/icons/discover.svg",
+      route: "/discover",
+      label: "Discover",
+    },
+    {
+      imgURL: "/icons/microphone.svg",
+      route: "/create-podcast",
+      label: "Create Podcast",
+    },
+  ];
+  
+  export const voiceDetails = [
+    {
+      id: 1,
+      name: "alloy",
+    },
+    {
+      id: 2,
+      name: "echo",
+    },
+    {
+      id: 3,
+      name: "fable",
+    },
+    {
+      id: 4,
+      name: "onyx",
+    },
+    {
+      id: 5,
+      name: "nova",
+    },
+    {
+      id: 6,
+      name: "shimmer",
+    },
+  ];
+  
+  export const podcastData = [
+    {
+      id: 1,
+      title: "The Joe Rogan Experience",
+      description: "A long form, in-depth conversation",
+      imgURL:
+        "https://beaming-shark-99.convex.cloud/api/storage/364013fb-a8fc-4304-b0c4-80091b236622",
+    },
+    {
+      id: 2,
+      title: "The Futur",
+      description: "This is how the news should sound",
+      imgURL:
+        "https://beaming-shark-99.convex.cloud/api/storage/bfad80f0-6898-4924-8b91-1880cbe7124e",
+    },
+    {
+      id: 3,
+      title: "Waveform",
+      description: "Join Michelle Obama in conversation",
+      imgURL:"https://beaming-shark-99.convex.cloud/api/storage/02a1bd1d-c938-4554-a2f4-a9907b968353",
+    },
+    {
+      id: 4,
+      title: "The Tech Talks Daily Podcast",
+      description: "This is how the news should sound",
+      imgURL:"https://beaming-shark-99.convex.cloud/api/storage/ad95c690-2813-4d92-a812-6ee2b17c813c",
+    },
+    {
+      id: 5,
+      title: "GaryVee Audio Experience",
+      description: "A long form, in-depth conversation",
+      imgURL:"https://beaming-shark-99.convex.cloud/api/storage/5e427f2d-c622-49d5-b162-1228107fee5a",
+    },
+    {
+      id: 6,
+      title: "Meri Podcast ",
+      description: "Join Solomon Kassa in conversation",
+      imgURL:"https://beaming-shark-99.convex.cloud/api/storage/20d5386f-8308-474c-8ea2-c6847b71ea2d"
+    },
+    {
+      id: 7,
+      title: "IMPAULSIVE",
+      description: "A long form, in-depth conversation",
+      imgURL:"https://beaming-shark-99.convex.cloud/api/storage/4450e918-590f-49b8-944a-daa07d9ddb4d"
+    },
+    {
+      id: 8,
+      title: "Ted Tech",
+      description: "This is how the news should sound",
+      imgURL:
+        "https://beaming-shark-99.convex.cloud/api/storage/4d972f47-f1f5-4166-9eb4-f03682320474",
+    },
+  ];
