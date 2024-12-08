3666d90eb711b25fbdcf3f27dac4e92030ebbf55
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-08 14:22:46 +0300

    add right side bar

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return <section className="right_sidedbar">
+    <h1 className='text-20 font-bold text-white-1'>
+          Trending Podcasts
+
+        </h1>
+  </section>;
+};
+
+export default RightSidebar;
