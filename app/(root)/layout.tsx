4722afa588b0168c0818feeb9017e9311247ea3a
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-14 17:42:07 +0300

    add toaster

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import "../globals.css";
 import Image from "next/image";
 import RightSidebar from "@/components/RightSidebar";
 import MobileNav from "@/components/MobileNav";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function RootLayout({
   children,
@@ -26,7 +27,7 @@ export default function RootLayout({
               <MobileNav />
             </div>
             <div className="flex flex-col md:pb-14">
-              {/* TODO toaster */}
+              <Toaster />
               {children}
             </div>
           </div>
