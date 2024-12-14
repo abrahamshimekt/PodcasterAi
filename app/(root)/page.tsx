56d2c8a2d1df00ae5a76d459410fe031a872fa0a
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-14 17:43:27 +0300

    fetch trending podcasts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,27 @@
 "use client";
 import PodcastCard from "@/components/PodcastCard";
-import { podcastData } from "@/constants";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import React from "react";
 
 const page = () => {
-  const tasks = useQuery(api.tasks.get);
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
   return (
     <div className="mt-9 bg-black-1 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
         <div className="podcast_grid">
-          {podcastData.map(({ id, title, description, imgURL }) => (
-            <PodcastCard
-              key={id}
-              imgUrl={imgURL}
-              title={title}
-              description={description}
-              podcastId={id}
-            />
-          ))}
+          {trendingPodcasts?.map(
+            ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+              <PodcastCard
+                key={_id}
+                imgUrl={imageUrl!}
+                title={podcastTitle}
+                description={podcastDescription}
+                podcastId={_id!}
+              />
+            )
+          )}
         </div>
       </section>
     </div>
