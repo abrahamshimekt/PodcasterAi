a25b62260d9eb89938543c4c7c27015a06d26d19
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-14 17:52:10 +0300

    remove text

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const RightSidebar = () => {
-  return <section className="right_sidedbar">
-    <h1 className='text-20 font-bold text-white-1'>
-          Trending Podcasts
-
-        </h1>
-  </section>;
+  return (
+    <section className="right_sidedbar">
+      <h1 className="text-20 font-bold text-white-1">
+        
+      </h1>
+    </section>
+  );
 };
 
 export default RightSidebar;
