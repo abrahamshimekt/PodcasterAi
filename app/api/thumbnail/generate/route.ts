331c2216ce7885763887a06f4ebb7b2252a983fe
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-14 17:46:50 +0300

    add image generator api from pollinations

---
+++ app/api/thumbnail/generate/route.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+export async function POST(req: Request) {
+  try {
+    const { prompt } = await req.json();
+    const response = await axios.get(
+      `https://image.pollinations.ai/prompt/${prompt}`,
+      { responseType: "arraybuffer" }
+    );
+    return new NextResponse(response.data, {
+      headers: {
+        "Content-Type": "application/octet-stream",
+      },
+    });
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ error: "Failed to fetch image" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
