d04ed1b90dcffc1cd0e01ae32699f19813d49f3e
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2024-12-17 11:03:50 +0300

    move convex clerk provider to the root (#1)
    LGTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import ConvexClerkProvider from "./providers/convexClerkProvider";
+import ConvexClerkProvider from "../providers/convexClerkProvider";
 export const metadata: Metadata = {
   title: "Podcaster",
   description: "Generate you podcasts using AI",
