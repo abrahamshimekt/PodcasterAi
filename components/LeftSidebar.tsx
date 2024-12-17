860b2d180dc25b8ae825dd4e8ea6e50c773e8c54
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-17 10:53:07 +0300

    add login and log out

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const { signOut } = useClerk();
   return (
-    <div className="left_sidebar">
+    <section className="left_sidebar">
       <div className="flex flex-col gap-6">
         <Link
           href="/"
@@ -23,12 +26,16 @@ const LeftSidebar = () => {
           </h1>
         </Link>
         {sidebarLinks.map(({ route, label, imgURL }) => {
-            const isActive = pathname === route || pathname.startsWith(`${route}/`)
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
           return (
             <Link
               href={route}
               key={label}
-              className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",{"bg-nav-focus border-r-4 border-orange-1":isActive})}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+              )}
             >
               <Image src={imgURL} alt={label} width={24} height={24} />
               {label}
@@ -36,7 +43,28 @@ const LeftSidebar = () => {
           );
         })}
       </div>
-    </div>
+      <SignedOut>
+        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+          <Button
+            asChild
+            className="text-16 w-full bg-orange-1 font-extrabold hover:!bg-orange-600"
+          >
+            <Link href="/sign-in"> Sign in</Link>
+          </Button>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+          <Button
+         
+            className="text-16 w-full bg-orange-1 font-extrabold hover:!bg-orange-600"
+            onClick={() => signOut(() => router.push("/"))}
+          >
+            Log Out
+          </Button>
+        </div>
+      </SignedIn>
+    </section>
   );
 };
 
