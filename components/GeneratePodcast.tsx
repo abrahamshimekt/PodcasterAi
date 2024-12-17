76b6817e71420237982a60844f64aa82706762a6
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-17 10:52:32 +0300

    make hover bg of button important

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -80,7 +80,7 @@ const GeneratePodcast = (props: GeneratePodcastProps) => {
       <div className="mt-5 w-full max-w-[200px]">
         <Button
           type="submit"
-          className="text-16  bg-orange-1 py-4 font-bold text-white-1"
+          className="text-16  bg-orange-1 py-4 font-bold text-white-1 hover:!bg-orange-600"
           onClick={generatePodcast}
         >
           {isGenerating ? (
