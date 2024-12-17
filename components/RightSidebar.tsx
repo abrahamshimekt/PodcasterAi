74046ac88d7511ccd041d2e348584aec78166a1a
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-17 10:55:28 +0300

    add carousel and top podcaster

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,11 +1,67 @@
+"use client";
+import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Header from "./Header";
+import Carousel from "./Carousel";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useRouter } from "next/navigation";
+import LoaderSpinner from "./LoaderSpinner";
 
 const RightSidebar = () => {
+  const router = useRouter();
+  const { user } = useUser();
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
+  if(!topPodcasters) return <LoaderSpinner/>
   return (
-    <section className="right_sidedbar">
-      <h1 className="text-20 font-bold text-white-1">
-        
-      </h1>
+    <section className="right_sidebar text-white-1">
+      <SignedIn>
+        <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
+          <UserButton />
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-16 truncate">{user?.firstName}</h1>
+            <h1 className="text-16 truncate">{user?.lastName}</h1>
+            <Image
+              src="/icons/right-arrow.svg"
+              alt="arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
+      </SignedIn>
+      <section>
+        <Header headerTitle="Fans Like You" titleClassName="" />
+        <Carousel fansLikeDetail={topPodcasters!} />
+      </section>
+      <section className="flex flex-col gap-8 pt-12">
+        <Header headerTitle="Top Podcasters" titleClassName="" />
+        <div className="flex flex-col gap-6">
+          {topPodcasters?.slice(0, 4).map((podcaster) => (
+            <div
+              key={podcaster._id}
+              className="flex cursor-pointer justify-between"
+              onClick={() => router.push(`/profile/${podcaster.clerkId}`)}
+            >
+              <figure className="flex items-center gap-2">
+                <Image
+                  src={podcaster.imageUrl}
+                  alt={podcaster.name}
+                  width={44}
+                  height={44}
+                  className="aspect-square rounded-lg"
+                />
+                <h2 className="text-14 font-semibold text-white-1">
+                  {podcaster.name}
+                </h2>
+              </figure>
+              <p className="text-12 font-normal">{podcaster?.totalPodcasts} podcasts</p>
+            </div>
+          ))}
+        </div>
+      </section>
     </section>
   );
 };
