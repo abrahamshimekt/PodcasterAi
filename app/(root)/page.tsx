61d36a763a51577a87e2282fd55a86d297df9a98
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-12-20 22:37:55 +0300

    fix overflow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 const page = () => {
   const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
   return (
-    <div className="mt-9 bg-black-1 flex flex-col gap-9">
+    <div className="mt-9 bg-black-1 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
         <div className="podcast_grid">
